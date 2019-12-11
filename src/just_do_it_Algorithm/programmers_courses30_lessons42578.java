e4e727e770a2164e90db3fb779f57b365c32352d
dudcheol <parkyc0301@gmail.com>
2019-12-11 20:46:53 +0900

    풀이봄

--- src/just_do_it_Algorithm/programmers_courses30_lessons42578.java
+++ src/just_do_it_Algorithm/programmers_courses30_lessons42578.java
@@ -15,34 +15,38 @@ public class programmers_courses30_lessons42578 {
 			} else {
 				map.put(currentKey, 1);
 			}
+		}	
+		
+		int answer = 1;
+		
+		for(int val : map.values()) {
+			answer *= (val+1);
 		}
+		
+		return answer-1;
 
-		System.out.println(map);
-
-		Iterator<String> mapIter = map.keySet().iterator();
-
-		int speciesCnt = map.size();
-//		System.out.println("speciesCnt:" + speciesCnt);
-		int result = 0;
-
-		// n개 중 2..max 선택
-		for (int i = 2; i <= speciesCnt; i++) {
-			int temp = nCr(speciesCnt, i);
-			// 한 종류에 여러개가 있다면 그 만큼 경우의 수가 늘어나므로 곱해준다
-			while (mapIter.hasNext()) {
-				temp *= map.get(mapIter.next());
-				System.out.println(temp);
-			}
-			mapIter = map.keySet().iterator();
-			result += temp;
-			System.out.println("temp = "+temp);
-			System.out.println("result = "+result);
-		}
-
-		// 각각 하나씩만 입었을 경우 더함
-		result += clothes.length;
-
-		return result;
+//		Iterator<String> mapIter = map.keySet().iterator();
+//
+//		int speciesCnt = map.size();
+////		System.out.println("speciesCnt:" + speciesCnt);
+//		int result = 0;
+//
+//		// n개 중 2..max 선택
+//		for (int i = 2; i <= speciesCnt; i++) {
+//			int temp = nCr(speciesCnt, i);
+//			// 한 종류에 여러개가 있다면 그 만큼 경우의 수가 늘어나므로 곱해준다
+//			while (mapIter.hasNext()) {
+//				temp *= map.get(mapIter.next());
+//				System.out.println(temp);
+//			}
+//			mapIter = map.keySet().iterator();
+//			result += temp;
+//			System.out.println("temp = "+temp);
+//			System.out.println("result = "+result);
+//		}
+//
+//		// 각각 하나씩만 입었을 경우 더함
+//		result += clothes.length;
 	}
 
 	// 조합 공식 사용 nCr = n! / r!(n-r)!
