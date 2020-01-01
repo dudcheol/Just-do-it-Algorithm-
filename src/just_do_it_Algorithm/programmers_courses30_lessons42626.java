c9311a03c4d461435d6935becdaa3654fe190ecb
dudcheol <parkyc0301@gmail.com>
2020-01-01 13:10:43 +0900

    프로그래머스 - 코딩테스트 연습 - 힙 - 42626 - 더 맵게 - 힙 사용하지 않고 풀어보려고 했는데,, 힙을 사용하는게 나을듯. 문제를 보고 힙을 써야하는 문제인지를 먼저 파악해야함

---
+++ src/just_do_it_Algorithm/programmers_courses30_lessons42626.java
@@ -0,0 +1,52 @@
+package just_do_it_Algorithm;
+
+import java.util.*;
+
+/*
+ * 12:15 start
+ * 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+ * Leo는 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복하여 섞습니다.
+ * 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수를 return
+ * 
+ * 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return 합니다.
+ */
+
+public class programmers_courses30_lessons42626 {
+	static int solution(int[] scoville, int K) {
+        int answer = 0;
+        ArrayList<Integer> scovList = new ArrayList<>();
+        
+        // 가장 맵지 않은 순서로 정렬
+        int size = scoville.length;
+        
+        // 리스트로 만듬
+        for(int scov : scoville) {
+        	scovList.add(scov);
+        }
+        
+        while(true) {
+			Collections.sort(scovList);
+        	int resultScov = scovList.get(0) + scovList.get(1)*2;
+        	answer++;
+        	scovList.remove(0);
+        	scovList.remove(0);
+        	scovList.add(0, resultScov);
+        	if(resultScov < K) {
+        		continue;
+        	} else {
+        		if(scovList.get(1) < K) {
+        			continue;
+        		} else break;
+        	}
+        }
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		int[] scoville = {1, 2, 3, 9, 10, 12};
+		int K = 7;
+		
+		System.out.println(solution(scoville, K));
+	}
+}
