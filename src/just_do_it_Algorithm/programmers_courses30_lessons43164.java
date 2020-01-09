8d615a019dc8b9be072132a076795d984ee41a87
dudcheol <parkyc0301@gmail.com>
2020-01-09 22:24:56 +0900

    프로그래머스 - 코딩테스트 연습 - DFS/BFS - 43164 - 여행경로 정답여부는 모르겠음. ide에서는 제대로 나오는데 웹에서 실행하면 Ts.next에 값이 안들어감..;;

--- src/just_do_it_Algorithm/programmers_courses30_lessons43164.java
+++ src/just_do_it_Algorithm/programmers_courses30_lessons43164.java
@@ -3,7 +3,9 @@ package just_do_it_Algorithm;
 import java.util.*;
 
 /*
- * 20:30 시작
+ * 20:30 시작 ~ 22:00 마무리 -- 틀림 // 1시간 30분
+ * 다음날 23:00 시작 ~ 02:00 못품..
+ * 다음날 18:30 시작 ~ 19:30 풀었따!
  * 방문하는 공항 경로를 배열에 담아 return 하도록 solution 함수를 작성
  * tickets의 각 행 [a, b]는 a 공항에서 b 공항으로 가는 항공권이 있다는 의미
  * - 모든 공항은 알파벳 대문자 3글자로 이루어집니다
@@ -13,80 +15,102 @@ import java.util.*;
  */
 
 public class programmers_courses30_lessons43164 {
+	static Ticket[] Ts;
+	static ArrayList<String> answerL;
+	
 	static class Ticket {
 		String start;
 		String dest;
 		boolean visited;
+		ArrayList<Ticket> next;
+
 		Ticket(String[] ticket) {
 			this.start = ticket[0];
 			this.dest = ticket[1];
 			this.visited = false;
+			this.next = new ArrayList<>();
 		}
+		
+		void setNext(Ticket t) {
+			this.next.add(t);
+		}
+	}
+
+	static void dfs(Ticket now) {
+		if(now.visited) return;
+		for(int i=0;i<now.next.size();i++) {
+			if(now.next.get(i).visited) continue;
+			now.visited = true;
+			dfs(now.next.get(i));
+			for(int j=0;j<Ts.length;j++) {
+				if(!Ts[j].visited) {
+					now.visited = false;
+					return;
+				}
+			}
+		}
+		now.visited = true;
+		answerL.add(now.dest);
 	}
 
 	static String[] solution(String[][] tickets) {
-		String[] answer = {};
-		int size = tickets.length;
-		Ticket[] tAry = new Ticket[size];
-		ArrayList<String> answerL = new ArrayList<>();
-		Queue<Ticket> q = new LinkedList<>();
-		Ticket bestWay = null; // 가능한 경로가 2개 이상일 경우 알파벳순서 앞서는 경로 채택
-		Ticket target = null; // 다음 티켓
+		String[] answer = new String[tickets.length+1];
+		int tSize = tickets.length;
+		answerL = new ArrayList<>();
+		Ts = new Ticket[tSize];
 
-		// 티켓 객체로 티켓 새롭게 저장
-		for (int i = 0; i < size; i++) {
-			tAry[i] = new Ticket(tickets[i]);
-			// 출발티켓 선정
-			if (tAry[i].start.equals("ICN")) {
-				if (target == null || tAry[i].dest.compareTo(target.dest) < 0) { // 초기화
-					target = tAry[i];
+		for (int i = 0; i < tSize; i++) {
+			Ts[i] = new Ticket(tickets[i]);
+		}
+
+		// 티켓을 문자열을 기준으로 오름차순 정렬
+		Arrays.sort(Ts, new Comparator<Ticket>() {
+			@Override
+			public int compare(Ticket o1, Ticket o2) {
+				if (o1.start.compareTo(o2.start) > 0)
+					return 1;
+				else if (o1.start.compareTo(o2.start) < 0)
+					return -1;
+				else {
+					if (o1.dest.compareTo(o2.dest) >= 0)
+						return 1;
+					else
+						return -1;
+				}
+			}
+		});
+		
+		
+		// 트리화하기
+		for(int i=0;i<tSize;i++) {
+			for(int j=0;j<tSize;j++) {
+				if(Ts[i].dest == Ts[j].start) {
+					Ts[i].setNext(Ts[j]);
 				}
 			}
 		}
 		
-		Arrays.sort(tickets);
-		System.out.println(tickets);
-		
-		target.visited = true;
-		answerL.add(target.start);
-		answerL.add(target.dest);
-		while (true) {
-			// 티켓을 큐에 모두 삽입
-			for (int i = 0; i < size; i++) {
-				if (!tAry[i].visited) // 이미 방문했으면 큐에 삽입하지 않음
-					q.offer(tAry[i]);
+		// 첫번째 티켓을 찾고 그것으로 dfs실행
+		for(Ticket t : Ts) {
+			if(t.start.equals("ICN")) {
+				dfs(t);
 			}
-
-			if (q.isEmpty())
-				return answerL.toArray(new String[answerL.size()]);
-
-			while (!q.isEmpty()) {
-				Ticket p = q.poll();
-				// 만일 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로 선택
-				if (p.start.equals(target.dest)) { // 다음 출발지와 같을 경우
-					if (bestWay == null)
-						bestWay = p;
-					else {
-						if (bestWay.start.compareTo(p.start) < 0) { // 가능한경로 2개 이상인데 새로 뽑은 애가 알파벳순서가 더 앞에있다면
-							bestWay = p;
-						}
-					}
-				}
-			}
-			if(bestWay == null) {
-				
-			}
-			answerL.add(bestWay.dest);
-			bestWay.visited = true; // 방문한 곳으로 처리
-			target = bestWay;
-			bestWay = null;
 		}
+		
+		// 답
+		answer[0] = "ICN";
+		for(int i=1;i<=answerL.size();i++) {
+			answer[i] = answerL.get(answerL.size()-i);
+		}
+
+		return answer;
 	}
 
 	public static void main(String[] args) {
-//		String[][] tickets = { { "ICN", "JFK" }, { "HND", "IAD" }, { "JFK", "HND" } };
+		String[][] tickets = { { "ICN", "JFK" }, { "HND", "IAD" }, { "JFK", "HND" } };
 //		String[][] tickets = { { "ICN", "JFK" }, { "JFK", "HND" } };
-		String[][] tickets = { { "ICN", "SFO" }, { "ICN", "ATL" }, { "SFO", "ATL" }, { "ATL", "ICN" },{ "ATL", "SFO" } };
+//		String[][] tickets = { { "ICN", "SFO" }, { "ICN", "ATL" }, { "SFO", "ATL" }, { "ATL", "ICN" },
+//				{ "ATL", "SFO" } };
 //		String[][] tickets = { { "ICN", "COO" }, { "ICN", "BOO" }, { "COO", "ICN" }, { "BOO", "DOO" } };
 
 		for (String sol : solution(tickets)) {
