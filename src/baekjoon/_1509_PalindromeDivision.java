ba03813e4d2bf887eaf5458ee193b6ecd883d973
dudcheol <parkyc0301@gmail.com>
2020-01-21 23:08:46 +0900

    백준 - 1509 - 팰린드롬 분할 - 공부가 더 필요함

---
+++ src/baekjoon/_1509_PalindromeDivision.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+/*
+ * 백준 - 1509 - 팰린드롬 분할
+ * 시작:20:45
+ * 끝:
+ * 시간:
+ */
+
+import java.util.*;
+
+public class _1509_PalindromeDivision {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String ex = sc.nextLine();
+		System.out.println(ex);
+		
+		int answer = 0;
+		int size = ex.length();
+		System.out.println(size);
+		System.out.println(ex.charAt(size/2));
+		
+		
+	}
+}
