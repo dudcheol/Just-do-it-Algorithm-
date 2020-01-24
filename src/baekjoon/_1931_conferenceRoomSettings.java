bccc03dcaccecf6c6fc68ee9194ad812aa7fe3f5
dudcheol <parkyc0301@gmail.com>
2020-01-24 13:10:30 +0900

    백준 - 그리디 - 1931 - 회의실배정 - 오답 : 메모리초과

--- src/baekjoon/_1931_conferenceRoomSettings.java
+++ src/baekjoon/_1931_conferenceRoomSettings.java
@@ -13,12 +13,10 @@ public class _1931_conferenceRoomSettings {
 	static class Meeting {
 		int startTime;
 		int endTime;
-		int runningTime;
 
 		Meeting(int s, int e) {
 			this.startTime = s;
 			this.endTime = e;
-			this.runningTime = endTime - startTime;
 		}
 	}
 
@@ -26,12 +24,7 @@ public class _1931_conferenceRoomSettings {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		ArrayList<Meeting> m = new ArrayList<>();
-//		Meeting[] m = new Meeting[n];
-//		int[] st = new int[n];
-//		int[] ed = new int[n];
 		for (int i = 0; i < n; i++) {
-//			st[i] = sc.nextInt();
-//			ed[i] = sc.nextInt();
 			m.add(new Meeting(sc.nextInt(), sc.nextInt()));
 		}
 
@@ -42,29 +35,10 @@ public class _1931_conferenceRoomSettings {
 				fe = m.get(i);
 			}
 		}
-		System.out.println("가장빨리끝나는회의는:" + fe.endTime);
+		System.out.println("가장빨리끝나는회의는:" + fe.startTime+","+fe.endTime);
 		m.remove(fe);
 
-		// fe와 가장 근접한 시간에 시작하는 회의를 찾되, 회의시간이 가장 짧은 회의가 더 우선순위
-		// 우선, 회의시간이 짧은 순서로 정렬
-		Collections.sort(m, new Comparator<Meeting>() {
-			@Override
-			public int compare(Meeting o1, Meeting o2) {
-				if (o1.startTime > o2.startTime) {
-					return 1;
-				} else if (o1.startTime < o2.startTime) {
-					return -1;
-				} else {
-					if (o1.runningTime > o2.runningTime) {
-						return 1;
-					} else if (o1.runningTime < o2.runningTime) {
-						return -1;
-					} else
-						return 0;
-				}
-			}
-		});
-
+		// fe가 끝나는 시간에 시작하는 회의 중 가장 빨리 끝나는 시간 찾기
 		int answer = 1;
 		Meeting cur = fe;
 		ArrayList<Meeting> can = new ArrayList<>();
@@ -76,11 +50,12 @@ public class _1931_conferenceRoomSettings {
 					System.out.println("가능한애는 " + m.get(i).startTime + "," + m.get(i).endTime);
 				}
 			}
-			// 가능한애들 중에 회의시간이 가장 짧은애 선택
+			// 가능한애들 중에 회의시간이 가장 짧은애 선택이 아니고!!!!!!!!!!!!!!
+			// 가장 빨리 끝나는  회의를 선택한다!!!!!!!!!!!!!!!!!!!!!!!!!!!
 			if(can.size()==0) break;
 			Meeting _fe = can.get(0);
 			for (int i = 1; i < can.size(); i++) {
-				if (_fe.runningTime > can.get(i).runningTime) {
+				if (_fe.endTime > can.get(i).endTime) {
 					_fe = can.get(i);
 				}
 			}
@@ -92,11 +67,6 @@ public class _1931_conferenceRoomSettings {
 			cur=_fe;
 			System.out.println("------------");
 		}
-
-//		cur=m.get(i);
-//		answer++;
-//		m.remove(cur);
-//		i=-1;
 
 		System.out.println(answer);
 	}
