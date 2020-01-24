de587bf9380b4f346da11272a0778458beb414b2
dudcheol <parkyc0301@gmail.com>
2020-01-24 14:15:19 +0900

    백준 - 그리디 - 1931 - 회의실배정 - 정답!! 치명적 실수 : 참조에 의한 호출  * [배운 것]  * - sort 쓸 때, compare사용하면 좋다. 그냥은 오름차순, -붙이면 내림차순 된다  * - Comparator쓸 때, 만약 특정 클래스에 대해서 정렬하고 싶다면 해당 클래스를 implements Comparable해서 써도됨  ********************* ********************* [주의해야할 것] ********************* *********************  * - 값에 의한 호출이 아닌 "참조에 의한 호출" 반드시 조심하자!! 이런 실수 때문에 시간 너무 많이 잡아먹는다.  *      참조에 의해 호출해버리면 해당 값을 변경할 때 참조한 곳까지 값이 변경되므로 나중에 발견하기 힘든 에러가 될 수 있다.

--- src/baekjoon/_1931_conferenceRoomSettings.java
+++ src/baekjoon/_1931_conferenceRoomSettings.java
@@ -3,8 +3,15 @@ package baekjoon;
 /*
  * 백준 - 그리디 - 1931 - 회의실배정
  * 시작: 11:45
- * 끝:
- * 시간:
+ * 끝: 14:07
+ * 시간: 2시간 22분
+ * [배운 것]
+ * - sort 쓸 때, compare사용하면 좋다. 그냥은 오름차순, -붙이면 내림차순 된다
+ * - Comparator쓸 때, 만약 특정 클래스에 대해서 정렬하고 싶다면 해당 클래스를 implements Comparable해서 써도됨
+ * 
+ * [주의해야할 것]
+ * - 값에 의한 호출이 아닌 "참조에 의한 호출" 반드시 조심하자!! 이런 실수 때문에 시간 너무 많이 잡아먹는다.
+ * 	 참조에 의해 호출해버리면 해당 값을 변경할 때 참조한 곳까지 값이 변경되므로 나중에 발견하기 힘든 에러가 될 수 있다.
  */
 
 import java.util.*;
@@ -13,12 +20,10 @@ public class _1931_conferenceRoomSettings {
 	static class Meeting {
 		int startTime;
 		int endTime;
-		int runningTime;
 
 		Meeting(int s, int e) {
 			this.startTime = s;
 			this.endTime = e;
-			this.runningTime = endTime - startTime;
 		}
 	}
 
@@ -26,77 +31,44 @@ public class _1931_conferenceRoomSettings {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		ArrayList<Meeting> m = new ArrayList<>();
-//		Meeting[] m = new Meeting[n];
-//		int[] st = new int[n];
-//		int[] ed = new int[n];
 		for (int i = 0; i < n; i++) {
-//			st[i] = sc.nextInt();
-//			ed[i] = sc.nextInt();
 			m.add(new Meeting(sc.nextInt(), sc.nextInt()));
 		}
-
+		
+/*		실수해서 1시간 넘게 잡아먹은 부분
+ * 
 		// 가장 빨리 끝나는 회의 찾기
 		Meeting fe = m.get(0);
 		for (int i = 1; i < n; i++) {
 			if (fe.endTime > m.get(i).endTime) {
-				fe = m.get(i);
+				fe = m.get(i); // <- fe는 현재 m.get(i)의 값을 저장한게 아니라 참조하고 있는 것이다
+							   //	 따라서, fe의 값을 바꾸면 m.get(i)의 값도 함께 바뀐다.
 			}
 		}
 		System.out.println("가장빨리끝나는회의는:" + fe.endTime);
-		m.remove(fe);
+*
+*/
 
-		// fe와 가장 근접한 시간에 시작하는 회의를 찾되, 회의시간이 가장 짧은 회의가 더 우선순위
-		// 우선, 회의시간이 짧은 순서로 정렬
+		// 빨리 끝나는 순서로 정렬하되, 그 안에는 시작시간에 따른 오름차순으로 정렬
 		Collections.sort(m, new Comparator<Meeting>() {
 			@Override
 			public int compare(Meeting o1, Meeting o2) {
-				if (o1.startTime > o2.startTime) {
-					return 1;
-				} else if (o1.startTime < o2.startTime) {
-					return -1;
+				if(o1.endTime == o2.endTime) {
+					return Integer.compare(o1.startTime, o2.startTime);
 				} else {
-					if (o1.runningTime > o2.runningTime) {
-						return 1;
-					} else if (o1.runningTime < o2.runningTime) {
-						return -1;
-					} else
-						return 0;
+					return Integer.compare(o1.endTime, o2.endTime);
 				}
 			}
 		});
-
-		int answer = 1;
-		Meeting cur = fe;
-		ArrayList<Meeting> can = new ArrayList<>();
-		for (int k = 0; k < m.size(); k++) {
-			for (int i = 0; i < m.size(); i++) {
-				if (cur.endTime <= m.get(i).startTime) {
-					// 가능한애들
-					can.add(m.get(i));
-					System.out.println("가능한애는 " + m.get(i).startTime + "," + m.get(i).endTime);
-				}
+		
+		int answer=0;
+		int endTime = -1;
+		for(int i=0;i<m.size();i++) {
+			if(endTime <= m.get(i).startTime) {
+				endTime = m.get(i).endTime;
+				answer++;
 			}
-			// 가능한애들 중에 회의시간이 가장 짧은애 선택
-			if(can.size()==0) break;
-			Meeting _fe = can.get(0);
-			for (int i = 1; i < can.size(); i++) {
-				if (_fe.runningTime > can.get(i).runningTime) {
-					_fe = can.get(i);
-				}
-			}
-			System.out.println("선택:"+ _fe.startTime + "," + _fe.endTime);
-			can.clear();
-			m.remove(_fe);
-			answer++;
-			k=-1;
-			cur=_fe;
-			System.out.println("------------");
 		}
-
-//		cur=m.get(i);
-//		answer++;
-//		m.remove(cur);
-//		i=-1;
 
 		System.out.println(answer);
 	}
