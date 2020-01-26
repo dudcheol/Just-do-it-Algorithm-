706154e6e10dbdb3957876b18f4f113d1ee1caeb
dudcheol <parkyc0301@gmail.com>
2020-01-26 13:23:37 +0900

    프로그래머스 - 코딩테스트 연습 - DFS/BFS - 여행경로 - 정확성 75...

--- src/_unsolvedProblems/programmers_courses30_lessons43164.java
+++ src/_unsolvedProblems/programmers_courses30_lessons43164.java
@@ -1,118 +1,111 @@
 package _unsolvedProblems;
 
 import java.util.*;
-
 /*
  * 20:30 시작 ~ 22:00 마무리 -- 틀림 // 1시간 30분
  * 다음날 23:00 시작 ~ 02:00 못품..
  * 다음날 18:30 시작 ~ 19:30 풀었따!
- * 방문하는 공항 경로를 배열에 담아 return 하도록 solution 함수를 작성
- * tickets의 각 행 [a, b]는 a 공항에서 b 공항으로 가는 항공권이 있다는 의미
- * - 모든 공항은 알파벳 대문자 3글자로 이루어집니다
- * - 주어진 항공권은 모두 사용
- * - 만일 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 return 
- * 모든 도시를 방문할 수 없는 경우는 주어지지 않습니다
+ * 
+ * 20200126 시도
+ * 시작 : 10:50
+ * 끝 : 
+ * 시간 : 
  */
 
 public class programmers_courses30_lessons43164 {
-	static Ticket[] Ts;
-	static ArrayList<String> answerL;
+	private static ArrayList<String> answerL;
 
-	static class Ticket {
+	private static class Ticket {
 		String start;
 		String dest;
 		boolean visited;
-		ArrayList<Ticket> next;
 
 		Ticket(String[] ticket) {
 			this.start = ticket[0];
 			this.dest = ticket[1];
 			this.visited = false;
-			this.next = new ArrayList<>();
-		}
-
-		void setNext(Ticket t) {
-			this.next.add(t);
 		}
 	}
 
-	static void dfs(Ticket now) {
-		if (now.visited)
+	private static void dfs(ArrayList<Ticket> tl, Ticket now) {
+		if (now.visited) {
 			return;
-		for (int i = 0; i < now.next.size(); i++) {
-			if (now.next.get(i).visited)
-				continue;
-			now.visited = true;
-			dfs(now.next.get(i));
-			for (int j = 0; j < Ts.length; j++) {
-				if (!Ts[j].visited) {
-					now.visited = false;
-					return;
-				}
-			}
 		}
+		// 현재 티켓을 방문처리함
 		now.visited = true;
 		answerL.add(now.dest);
+
+		for (int i = 0; i < tl.size(); i++) {
+			if (tl.get(i).visited) {
+				continue;
+			}
+			// 현재 티켓의 도착지와 다음 티켓의 출발지가 같은 티켓을 dfs함
+			if (now.dest.equals(tl.get(i).start)) {
+				dfs(tl, tl.get(i));
+			}
+		}
+
+		// 주어진 항공권은 모두 사용해야 합니다.
+		for (Ticket t : tl) {
+			if (!t.visited) { // 티켓중 방문하지 않은 곳이 하나라도 있다면
+				answerL.remove(now.dest); // 정답에서 해당 티켓을 지우고
+				now.visited = false; // 티켓리스트에서 해당 티켓을 비방문 처리 한다
+				return;
+			}
+		}
+		// 티켓중 방문하지 않은 곳이 없다면
+		return;
 	}
 
-	static String[] solution(String[][] tickets) {
-		String[] answer = new String[tickets.length + 1];
-		int tSize = tickets.length;
+	public static String[] solution(String[][] tickets) {
 		answerL = new ArrayList<>();
-		Ts = new Ticket[tSize];
+		int tSize = tickets.length;
+		ArrayList<Ticket> tl = new ArrayList<>();
+		
+		String[][] sorted;
+		sorted = Arrays.stream(tickets)
+				.map(v -> v[0] + "," + v[1])
+				.distinct()
+				.sorted()
+				.map(v -> v.split(","))
+				.toArray(String[][]::new);
 
-		for (int i = 0; i < tSize; i++) {
-			Ts[i] = new Ticket(tickets[i]);
+		// tickets를 Ticket에 담는다
+		for (int i = 0; i < sorted.length; i++) {
+			tl.add(new Ticket(sorted[i]));
 		}
 
-		// 티켓을 문자열을 기준으로 오름차순 정렬
-		Arrays.sort(Ts, new Comparator<Ticket>() {
-			@Override
-			public int compare(Ticket o1, Ticket o2) {
-				if (o1.start.compareTo(o2.start) > 0)
-					return 1;
-				else if (o1.start.compareTo(o2.start) < 0)
-					return -1;
-				else {
-					if (o1.dest.compareTo(o2.dest) >= 0)
-						return 1;
-					else
-						return -1;
-				}
-			}
-		});
+		// 만일 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 return 합니다.
+//		Collections.sort(tl, new Comparator<Ticket>() {
+//			@Override
+//			public int compare(Ticket o1, Ticket o2) {
+//				if (o1.start.equals(o2.start)) {
+//					return o1.dest.compareTo(o2.dest);
+//				} else {
+//					return 0;
+//				}
+//			}
+//		});
 
-		// 트리화하기
-		for (int i = 0; i < tSize; i++) {
-			for (int j = 0; j < tSize; j++) {
-				if (Ts[i].dest == Ts[j].start) {
-					Ts[i].setNext(Ts[j]);
-				}
-			}
-		}
-
-		// 첫번째 티켓을 찾고 그것으로 dfs실행
-		for (Ticket t : Ts) {
-			if (t.start.equals("ICN")) {
-				dfs(t);
+		// 첫 출발지 찾기
+		// 항상 ICN 공항에서 출발합니다.
+		answerL.add("ICN");
+		for (int i = 0; i < tl.size(); i++) {
+			if (tl.get(i).start.equals("ICN")) {
+				dfs(tl, tl.get(i));
 			}
 		}
 
-		// 답
-		answer[0] = "ICN";
-		for (int i = 1; i <= answerL.size(); i++) {
-			answer[i] = answerL.get(answerL.size() - i);
-		}
-
-		return answer;
+		return answerL.toArray(new String[answerL.size()]);
 	}
 
 	public static void main(String[] args) {
-		String[][] tickets = { { "ICN", "JFK" }, { "HND", "IAD" }, { "JFK", "HND" } };
+//		String[][] tickets = { { "ICN", "JFK" }, { "HND", "IAD" }, { "JFK", "HND" } };
 //		String[][] tickets = { { "ICN", "JFK" }, { "JFK", "HND" } };
 //		String[][] tickets = { { "ICN", "SFO" }, { "ICN", "ATL" }, { "SFO", "ATL" }, { "ATL", "ICN" },
 //				{ "ATL", "SFO" } };
-//		String[][] tickets = { { "ICN", "COO" }, { "ICN", "BOO" }, { "COO", "ICN" }, { "BOO", "DOO" } };
+//		String[][] tickets = { { "COO", "ICN" },{ "COO", "ICN" }, { "ICN", "COO" }, { "ICN", "COO" }, { "ICN", "BOO" }, { "BOO", "DOO" } };
+		String[][] tickets = { { "COO", "ICN" }, { "COO", "ICN" }, { "ICN", "COO" } };
 
 		for (String sol : solution(tickets)) {
 			System.out.println(sol);
