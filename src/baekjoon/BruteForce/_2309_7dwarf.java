ef67c5758fa4eaf662602a6108a422b55fe4303d
dudcheol <parkyc0301@gmail.com>
2020-01-29 18:41:00 +0900

    [인터넷에서 검색함] Q: 이부분에서 꼭 전부 순회해야 하는가?? 에 대한 개선코드

--- src/baekjoon/BruteForce/_2309_7dwarf.java
+++ src/baekjoon/BruteForce/_2309_7dwarf.java
@@ -19,49 +19,88 @@ package baekjoon.BruteForce;
 import java.util.*;
 
 public class _2309_7dwarf {
-	static List<Integer> answer = new ArrayList<>();
+	 static int[] arr,visited;
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        arr = new int[9];
+	        visited = new int[9];
+	        for(int i=0;i<9;i++)
+	            arr[i] = sc.nextInt();
+	        solve(0,0,0);
+	    }
+	    
+	    // 키의 합은 100, 하나만 출력하면 되므로 출력후 flag로 빠저나온다.
+	    static boolean flag = false;
+	    private static void solve(int idx,int cnt,int sum) {
+	        if(flag) return;
+	        // 7개를 모았을 때
+	        if(cnt==7) {
+	            // 합이 100이라면 
+	            if(sum==100) {
+	                int[] result = new int[7];
+	                // 7개 체크된 값을 받아. 출력한다.
+	                for(int i=0,index=0;i<9;i++)
+	                    if(visited[i]==1) result[index++]=arr[i];
+	                Arrays.sort(result);
+	                for(int i:result)
+	                    System.out.println(i);
+	                flag = true;
+	            }
+	            return;
+	        }
+	        if(idx>=9) return;
+	        // 현재 포함
+	        visited[idx] = 1;
+	        solve(idx+1,cnt+1,sum+arr[idx]);
+	        visited[idx] = 0;
+	        
+	        // 현재 미포함
+	        solve(idx+1,cnt,sum);
+	    }
 
-	static boolean findDwarf(int tallSum, int cnt, int[] dwarfs, boolean[] visited) {
-		if (tallSum > 100 || cnt > 7)
-			return false;
-
-		if (cnt == 7) {
-			if (tallSum == 100) {
-				for (int i = 1; i < visited.length; i++) {
-					if (visited[i])
-						answer.add(dwarfs[i]);
-				}
-				return true;
-			}
-			return false;
-		}
-
-		for (int i = 1; i <= 9; i++) { // Q: 이부분에서 꼭 전부 순회해야 하는가??
-			if (visited[i])
-				continue;
-			tallSum += dwarfs[i];
-			visited[i] = true;
-			if(findDwarf(tallSum, cnt + 1, dwarfs, visited)) break;
-			tallSum -= dwarfs[i];
-			visited[i] = false;
-		}
-		
-		return false;
-	}
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int[] dwarfs = new int[10]; // 1부터 시작 ~9까지
-		for (int i = 1; i <= 9; i++) {
-			dwarfs[i] = sc.nextInt();
-		}
-
-		findDwarf(0, 0, dwarfs, new boolean[10]);
-
-		Collections.sort(answer);
-//		System.out.println("---------------");
-		for (int a : answer) {
-			System.out.println(a);
-		}
-	}
+//	static List<Integer> answer = new ArrayList<>();
+//
+//	static boolean findDwarf(int tallSum, int cnt, int[] dwarfs, boolean[] visited) {
+//		if (tallSum > 100 || cnt > 7)
+//			return false;
+//
+//		if (cnt == 7) {
+//			if (tallSum == 100) {
+//				for (int i = 1; i < visited.length; i++) {
+//					if (visited[i])
+//						answer.add(dwarfs[i]);
+//				}
+//				return true;
+//			}
+//			return false;
+//		}
+//
+//		for (int i = 1; i <= 9; i++) { // Q: 이부분에서 꼭 전부 순회해야 하는가??
+//			if (visited[i])
+//				continue;
+//			tallSum += dwarfs[i];
+//			visited[i] = true;
+//			if(findDwarf(tallSum, cnt + 1, dwarfs, visited)) break;
+//			tallSum -= dwarfs[i];
+//			visited[i] = false;
+//		}
+//		
+//		return false;
+//	}
+//
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int[] dwarfs = new int[10]; // 1부터 시작 ~9까지
+//		for (int i = 1; i <= 9; i++) {
+//			dwarfs[i] = sc.nextInt();
+//		}
+//
+//		findDwarf(0, 0, dwarfs, new boolean[10]);
+//
+//		Collections.sort(answer);
+////		System.out.println("---------------");
+//		for (int a : answer) {
+//			System.out.println(a);
+//		}
+//	}
 }
