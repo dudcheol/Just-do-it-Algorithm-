cb9f4e556dca5602e54a0b1f65d0ffacb3895c99
dudcheol <parkyc0301@gmail.com>
2020-03-09 14:11:41 +0900

    알고리즘 문제해결 전략 - 218p - 와일드카드 (완전탐색ver)  * - 교재참고! -  *    [간단한 해법]     문자열을 잘라가며 그것을 파라미터로 재귀를 진행한다. '*'를 기준으로 패턴을 나누는 것이 핵심!  *    [어떤  방식으로 접근했나?]     제한해둔 시간 (1시간30분)을 초과해서 교재를 참고했다  *    [해법을 찾는 데 결정적인 깨달음]     '*'을 기준으로 패턴을 나누는 것 자체를 생각하지 못했었다. 또, 재귀호출의 응용방법이 배웠던 부분에서만 생각이 가능했는데     이런식으로도 가능하다는 것을 깨달았다.  *    [다른 해결 방법이 있다면?]     DP를 이용하면 더욱 효율적인 프로그램이 완성된다

---
+++ src/APSS/dp/_218p_WildCard.java
@@ -0,0 +1,79 @@
+package APSS.dp;
+
+/*
+ * 알고리즘 문제해결 전략 - 218p - 와일드카드 (완전탐색ver)
+ * - 교재참고! -
+ *	[간단한 해법]
+	문자열을 잘라가며 그것을 파라미터로 재귀를 진행한다. '*'를 기준으로 패턴을 나누는 것이 핵심!
+ *	[어떤  방식으로 접근했나?]
+	제한해둔 시간 (1시간30분)을 초과해서 교재를 참고했다
+ *	[해법을 찾는 데 결정적인 깨달음]
+	'*'을 기준으로 패턴을 나누는 것 자체를 생각하지 못했었다. 또, 재귀호출의 응용방법이 배웠던 부분에서만 생각이 가능했는데
+	이런식으로도 가능하다는 것을 깨달았다.
+ *	[다른 해결 방법이 있다면?]
+	DP를 이용하면 더욱 효율적인 프로그램이 완성된다
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class _218p_WildCard {
+	static String wild;
+	static int strlen;
+	static String[] strs;
+	static List<String> answer;
+
+	static boolean findMatch(String w, String s) {
+		/*
+		 * '*'이후에 몇개의 문자가 등장할 지 알 수 없으므로 '*'을 기준으로 진행한다. '*'을 발견하면 발견 전의 문자열을 잘라내어 나머지
+		 * 문자열을 가지고 재귀를 진행한다.
+		 */
+		int pos = 0;
+		while (pos < w.length() && pos < s.length() && (w.charAt(pos) == '?' || w.charAt(pos) == s.charAt(pos))) {
+			pos++;
+		}
+
+		// 와일드카드의 문자(w) 끝에 도달했을때, 주어진 문자열(s)의 끝에도 도달한 것이라면 문자가 일치하는 것이므로 true
+		// 아니라면, w와 문자 수가 다른것이므로 false
+		if (pos == w.length()) {
+			return pos == s.length();
+		}
+
+		// '*'를 만나서 반복문이 종료된 경우 : '*'에 몇 글자를 대응해야 할 지를 '재귀호출'을 통해 확인한다
+		if (w.charAt(pos) == '*') {
+			for (int skip = 0; pos + skip <= s.length(); skip++) {
+				if (findMatch(w.substring(pos + 1), s.substring(pos + skip))) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(br.readLine());
+		for (int t = 0; t < tc; t++) {
+			wild = br.readLine();
+			strlen = Integer.parseInt(br.readLine());
+			strs = new String[strlen];
+			for (int i = 0; i < strlen; i++) {
+				strs[i] = br.readLine();
+			}
+
+			// 매치되는 파일명 찾기
+			answer = new ArrayList<>();
+			for (int i = 0; i < strlen; i++) {
+				if (findMatch(wild, strs[i]))
+					answer.add(strs[i]);
+			}
+
+			Collections.sort(answer);
+			
+			for (int i = 0; i < answer.size(); i++) {
+				System.out.println(answer.get(i));
+			}
+		}
+	}
+}
