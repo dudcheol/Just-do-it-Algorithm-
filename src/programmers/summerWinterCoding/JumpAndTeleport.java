5c81d5d3e6b03dcbc9540d6b16b87af1e8ca677a
dudcheol <parkyc0301@gmail.com>
2020-05-06 13:01:21 +0900

    프로그래머스 - 코딩테스트 연습 - Summer/Winter Coding - 점프와 순간 이동 - 정확성60/효율성40(풀이봄)  * 알고보니 정말 쉬운문제..  * 1부터 곱해가며 풀어나가려고 하니 정확성은 맞았어도 효율성에서 X  * n부터 나눠가며 풀어나가면 정말 쉬운 문제...

--- src/programmers/summerWinterCoding/JumpAndTeleport.java
+++ src/programmers/summerWinterCoding/JumpAndTeleport.java
@@ -1,38 +1,25 @@
 package programmers.summerWinterCoding;
 
-import java.util.Arrays;
-
+/**
+ * 알고보니 정말 쉬운문제..
+ * 1부터 곱해가며 풀어나가려고 하니 정확성은 맞았어도 효율성에서 X
+ * n부터 나눠가며 풀어나가면 정말 쉬운 문제...
+ */
 public class JumpAndTeleport {
-    static int N;
-    static int[][] cache;
 
     static int solution(int n) {
         int ans = 0;
 
-        N = n;
-        cache = new int[10000][10000];
-        for (int[] c : cache)
-            Arrays.fill(c, -1);
-        ans = minUse(1, 1);
-
-        return ans;
-    }
-
-    static int minUse(int pos, int use) {
-        // 기저
-        if (pos == N) {
-            return use;
-        } else if (pos > N) {
-            return Integer.MAX_VALUE;
+        while (n != 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else{
+                n -= 1;
+                ans++;
+            }
         }
 
-        if (cache[pos][use] != -1) return cache[pos][use];
-
-        cache[pos][use] = Integer.MAX_VALUE;
-        cache[pos][use] = Math.min(cache[pos][use], minUse(pos + 1, use + 1)); // 앞으로 한 칸 점프
-        cache[pos][use] = Math.min(cache[pos][use], minUse(pos * 2, use)); // 현재 위치의  x2 에 해당하는 위치로 순간이동
-
-        return cache[pos][use];
+        return ans;
     }
 
     public static void main(String[] args) {
