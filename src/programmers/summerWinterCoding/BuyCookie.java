d688b815428820c78af1495d1bec38f0acf98be5
dudcheol <parkyc0301@gmail.com>
2020-05-12 11:15:10 +0900

    프로그래머스 - 코딩테스트 연습 - Summer/Winter Coding - 쿠키 구입 - 필기

--- src/programmers/summerWinterCoding/BuyCookie.java
+++ src/programmers/summerWinterCoding/BuyCookie.java
@@ -1,9 +1,22 @@
 package programmers.summerWinterCoding;
 
+import java.util.Arrays;
+
+/**
+ * 현재 값을 기준으로 이전값과 이후값을 더해가며 문제를 푼다
+ * 처음에 생각은 했지만 안될것같아서 그만뒀는데 그게 정답이었다 ㅠㅠ
+ * 대강 짐작해보고 안될거같거니~ 싶으면 바로 다른 방법을 찾는 행동도 조심해야할 듯 싶다
+ * 대강 짐작하지말고 손으로 써가면서 짐작했던 것이 안되는 이유를 정확히하고 넘어가는 연습이 필요할듯
+ */
 public class BuyCookie {
+    static int[][] cache = new int[2001][2001];
 
     static int solution(int[] cookie) {
         int answer = -1;
+
+        for (int[] c : cache) {
+            Arrays.fill(c, -1);
+        }
 
         int sum = cookie[0];
         for (int i = 0; i < cookie.length - 1; i++) {
