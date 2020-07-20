82d0501936a7751532118f0f208e7bddd3505856
dudcheol <95_pyc@naver.com>
2020-07-20 23:37:02 +0900

    [ 자바스터디 01 ] - 추가공부

--- src/others/java/PrintBit.java
+++ src/others/java/PrintBit.java
@@ -1,13 +1,13 @@
-package others.java;
+    package others.java;
 
-public class PrintBit {
-    public static void main(String[] args) {
-        /* Byte값을 Bit로 출력하기 */
-        byte target = 3;
-        String trans;
-        System.out.println(trans = Integer.toBinaryString(target));
+    public class PrintBit {
+        public static void main(String[] args) {
+            /* Byte값을 Bit로 출력하기 */
+            byte target = 3;
+            String trans;
+            System.out.println(trans = Integer.toBinaryString(target));
 
-        /* Bit로 출력한 결과를 8자리로 맞추기 */
-        System.out.println(String.format("%08d", Integer.parseInt(trans)));
+            /* Bit로 출력한 결과를 8자리로 맞추기 */
+            System.out.println(String.format("%08d", Integer.parseInt(trans)));
+        }
     }
-}
