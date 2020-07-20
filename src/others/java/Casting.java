103a446c2e338b04155829ffe66dc674b8f76db6
박영철 <>
2020-07-20 23:34:03 +0900

    [ 자바스터디 01 ]

---
+++ src/others/java/Casting.java
@@ -0,0 +1,11 @@
+package others.java;
+
+public class Casting {
+    public static void main(String[] args) {
+        /* casting 크기비교 */
+        // byte < short < int < long < float < double
+        // 1 2 4 8 4 8 바이트 (1byte = 8bit)
+        // size로 보면 long이 float 보다 크지만, float는 소수점까지 저장할 수 있으므로 float이 더 크다고 본다.
+        // ex) 3.14f를 long으로 캐스팅하면 3이 저장된다.
+    }
+}
