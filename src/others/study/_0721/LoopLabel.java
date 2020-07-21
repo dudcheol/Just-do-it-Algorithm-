d871c28ca975de5b95804974185616b1b2bae2eb
dudcheol <95_pyc@naver.com>
2020-07-21 18:05:23 +0900

    [ 자바스터디 02 ]

---
+++ src/others/study/_0721/LoopLabel.java
@@ -0,0 +1,15 @@
+package others.study._0721;
+
+public class LoopLabel {
+    public static void main(String[] args) {
+        /* Label */
+        // here : label
+        here:
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (j == 3) break here; // 원래는 바로 위의 for문만 나오지만, label을 통해 break할 위치를 명시해주면 해당 for문이 break됨.
+                System.out.println(i + "," + j);
+            }
+        }
+    }
+}
