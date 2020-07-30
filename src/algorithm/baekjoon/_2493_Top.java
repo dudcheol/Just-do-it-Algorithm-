e044f64e4fd030806638b0799b66e91d4fa2b865
dudcheol <95_pyc@naver.com>
2020-07-30 21:45:41 +0900

    백준 / 골드5 /  탑 - 정답(풀이참고)

--- src/algorithm/baekjoon/_2493_Top.java
+++ src/algorithm/baekjoon/_2493_Top.java
@@ -3,8 +3,6 @@ package algorithm.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -13,47 +11,34 @@ public class _2493_Top {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        int[] answer = new int[n + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 스택 초기화
-        Stack<int[]> stack = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            stack.push(new int[]{i + 1, Integer.parseInt(st.nextToken())});
-        }
+        // 입력받으면서 진행하기
+        Stack<int[]> s = new Stack<>();
 
-        Queue<int[]> fails = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            int[] cur = {i, Integer.parseInt(st.nextToken())};
 
-        int[] cur = stack.pop();
-        while (!stack.isEmpty()) {
-            int[] target = stack.pop();
-
-            int size = fails.size();
-            for (int i = 0; i < size; i++) {
-                int[] fail = fails.poll();
-                if (fail[1] <= target[1]) {
-                    answer[fail[0]] = target[0];
+            while (!s.isEmpty()) {
+                if (s.peek()[1] < cur[1]) {
+                    // 스택의 top보다 현재 탑이 더 크다면,
+                    // 다음 탑들 중 현재 탑보다 큰 탑이 있을 경우에
+                    // 스택의 top보다도 당연히 클 것이므로 수신할 수 없는 탑이 된다.
+                    // 따라서 스택에서 제거
+                    s.pop();
                 } else {
-                    fails.offer(fail);
+                    // 스택의 top이 수신할 수 있는 탑이므로 해당 번호를 출력
+                    sb.append(s.peek()[0]).append(" ");
+                    break;
                 }
             }
 
-            if (cur[1] <= target[1]) {
-                // 송신성공
-                answer[cur[0]] = target[0];
-            } else {
-                // 송신실패
-                fails.offer(cur);
-            }
+            // 스택이 비어있으면 수신할 수 있는 탑이 없는 것이므로 0
+            if (s.isEmpty())
+                sb.append("0").append(" ");
 
-            cur = target;
+            s.push(cur);
         }
-
-        for (int i=1;i<=n;i++){
-            sb.append(answer[i]).append(" ");
-        }
-
         System.out.print(sb);
     }
 }
