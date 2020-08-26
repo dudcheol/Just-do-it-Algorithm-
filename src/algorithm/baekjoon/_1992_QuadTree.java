99aacac6391b06fd2040a9611ccb96bfe8e0908a
dudcheol <95_pyc@naver.com>
2020-08-26 11:47:16 +0900

    백준 / 실버1 / 쿼드트리 - 정답(개선) - 개선사항 : replace를 사용하지 않고 재귀내에서 문자열 완성 - 부족한점 : 47번줄의 part1.length() == 1 이라는 조건의 추가를 생각하기 어려움, 다른 풀이방법 필요

--- src/algorithm/baekjoon/_1992_QuadTree.java
+++ src/algorithm/baekjoon/_1992_QuadTree.java
@@ -6,58 +6,51 @@ import java.io.InputStreamReader;
 
 public class _1992_QuadTree {
     static int N;
-    static int[][] map;
-    static StringBuilder sb;
+    static char[][] map;
 
     public static void main(String[] args) throws IOException {
         //입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        sb = new StringBuilder();
+        map = new char[N][N];
 
         for (int i = 0; i < N; i++) {
             char[] tmp = br.readLine().toCharArray();
             for (int j = 0; j < N; j++) {
-                map[i][j] = tmp[j] - '0';
+                map[i][j] = tmp[j];
             }
         }
 
-        quadCompress(N, 1, 0, N - 1, 0, N - 1);
-
-        String answer = sb.toString();
-        while (answer.contains("(1111)")) {
-            answer = answer.replace("(1111)", "1");
-        }
-
-        while (answer.contains("(0000)")) {
-            answer = answer.replace("(0000)", "0");
-        }
-
-        System.out.println(answer);
+        System.out.println(quadCompress(N, 0, N - 1, 0, N - 1));
     }
 
     // 1 2
     // 3 4 의 4로 분할한다고 생각한다
-    private static void quadCompress(int size, int part, int rowStart, int rowEnd, int colStart, int colEnd) {
+    // return 타입을 String으로!
+    private static String quadCompress(int size, int rowStart, int rowEnd, int colStart, int colEnd) {
         if (size == 1) {
-            int ret = -1;
-            if (part == 1) ret = map[rowStart][colStart];
-            else if (part == 2) ret = map[rowStart][colEnd];
-            else if (part == 3) ret = map[rowEnd][colStart];
-            else if (part == 4) ret = map[rowEnd][colEnd];
-            sb.append(ret);
-            return;
+            // 굳이 part를 사용할 필요 없이 이 크기는 1이므로 rowStart, colStart가 map의 특정 인덱스가 됨
+            return Character.toString(map[rowStart][colStart]);
         }
-        sb.append("(");
+
         // 4분할 실시
         int midr = (rowStart + rowEnd) / 2;
         int midc = (colStart + colEnd) / 2;
-        quadCompress(size / 2, 1, rowStart, midr, colStart, midc); // 1
-        quadCompress(size / 2, 2, rowStart, midr, midc + 1, colEnd); // 2
-        quadCompress(size / 2, 3, midr + 1, rowEnd, colStart, midc); // 3
-        quadCompress(size / 2, 4, midr + 1, rowEnd, midc + 1, colEnd); // 4
-        sb.append(")");
+        int half = size / 2;
+        String part1 = quadCompress(half, rowStart, midr, colStart, midc); // 1
+        String part2 = quadCompress(half, rowStart, midr, midc + 1, colEnd); // 2
+        String part3 = quadCompress(half, midr + 1, rowEnd, colStart, midc); // 3
+        String part4 = quadCompress(half, midr + 1, rowEnd, midc + 1, colEnd); // 4
+
+        // part1.length() == 1이 있는 이유?
+        // length가 1보다 큰 경우에는 재귀적으로 쌓여온 문자열들을 가지고 있는 상태이므로 함부로 파트 2,3,4를 제외시킬 수 없다
+        if (part1.length() == 1 && part1.equals(part2) && part2.equals(part3) && part3.equals(part4)) {
+            return part1;
+        } else {
+            StringBuilder sb = new StringBuilder();
+            sb.append("(").append(part1).append(part2).append(part3).append(part4).append(")");
+            return sb.toString();
+        }
     }
 }
 
@@ -71,4 +64,6 @@ public class _1992_QuadTree {
 11110000
 11110011
 11110011
+
+answer: ((110(0101))(0010)1(0001))
  */
