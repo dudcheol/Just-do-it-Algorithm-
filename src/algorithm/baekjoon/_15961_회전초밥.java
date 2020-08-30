a443fc2e308b04ddfddf970e7480926cfe09a67c
dudcheol <95_pyc@naver.com>
2020-08-30 17:21:25 +0900

    백준 / 골드5 / 회전 초밥 - 풀이참고

--- src/algorithm/baekjoon/_15961_회전초밥.java
+++ src/algorithm/baekjoon/_15961_회전초밥.java
@@ -3,31 +3,72 @@ package algorithm.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class _15961_회전초밥 {
-    static int N, d, k, c; //접시의 수 N, 초밥의 가짓수 d, 연속해서 먹는 접시의 수 k, 쿠폰 번호 c
-    static int[] chobobs;
-    //2 ≤ N ≤ 3,000,000, 2 ≤ d ≤ 3,000, 2 ≤ k ≤ 3,000 (k ≤ N), 1 ≤ c ≤ d
 
     public static void main(String[] args) throws IOException {
         // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int N, d, k, c; //접시의 수 N, 초밥의 가짓수 d, 연속해서 먹는 접시의 수 k, 쿠폰 번호 c
+        //2 ≤ N ≤ 3,000,000, 2 ≤ d ≤ 3,000, 2 ≤ k ≤ 3,000 (k ≤ N), 1 ≤ c ≤ d
+
         N = Integer.parseInt(st.nextToken());
         d = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
 
-        chobobs = new int[N];
+        int[] input = new int[N];
         for (int i = 0; i < N; i++) {
-            chobobs[i] = Integer.parseInt(br.readLine());
+            input[i] = Integer.parseInt(br.readLine());
         }
 
-        Queue<Integer> q = new LinkedList<>();
+        int[] chobobs = new int[d + 1];
+
+        // 투포인터 알고리즘 사용
+        int max = 0;
+        int start = 0;
+        int end = k - 1;
+        int cnt = 0;
+
+        // start와 end 사이의 초밥 종류 초기화
+        for (int j = start; j <= end; j++) {
+            if (chobobs[input[j]] == 0) cnt++;
+            chobobs[input[j]]++;
+        }
+        max = Math.max(max, chobobs[c] == 0 ? cnt + 1 : cnt);
+
+        for (int i = k; i < N + k - 1; i++) {
+            // 포인터 한 칸 씩 오른쪽으로 이동
+            if (--chobobs[input[start]] == 0) {
+                // 같은 종류 없으므로 종류 - 1;
+                cnt--;
+            }
+
+            start++;
+            start %= N;
+            end++;
+            end %= N;
+
+            if (chobobs[input[end]]++ == 0) {
+                // 오른쪽으로 이동했는데 추가된 초밥이 지금까지 목록에 없다면
+                cnt++;
+            }
+
+            // 쿠폰을 이미 보유했는지 보유하지 않았는지에 따라 max 값 달라짐
+            max = Math.max(max, chobobs[c] == 0 ? cnt + 1 : cnt);
+        }
+
+        System.out.println(max);
+    }
+}
+
+/*
+잘못 푼 이유
+: 연속하면서 무조건 중복되는 초밥이 없어야 한다고 생각하여 잘못 품
+Queue<Integer> q = new LinkedList<>();
 
         int idx = 0;
         int answer = 0;
@@ -56,5 +97,4 @@ public class _15961_회전초밥 {
         }
 
         System.out.println(answer);
-    }
-}
+*/
