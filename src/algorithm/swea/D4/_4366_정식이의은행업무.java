766949bed4ee4c433bf80cb8008058e313efad59
dudcheol <95_pyc@naver.com>
2020-09-04 11:44:37 +0900

    swea / D4 / 정식이의 은행업무 - 정답(개선) - Math.pow를 사용하여 두개의 메소드를 하나로 합침 - 2^39, 3^39를 담을 수 있는 자료형으로 변경 (int -> long)

--- src/algorithm/swea/D4/_4366_정식이의은행업무.java
+++ src/algorithm/swea/D4/_4366_정식이의은행업무.java
@@ -8,7 +8,7 @@ public class _4366_정식이의은행업무 {
 	static int binSize;
 	static char[] third;
 	static int thrSize;
-	static char[] bAry = { '1', '0' };
+	static char[][] bAry = { { '1' }, { '0' } };
 	static char[][] tAry = { { '1', '2', '0' }, { '0', '2', '1' }, { '0', '1', '2' } };
 
 	public static void main(String[] args) throws Exception {
@@ -26,28 +26,28 @@ public class _4366_정식이의은행업무 {
 			// 경우의 수를 놓고 생각한다
 			// 한자리만 잘못 기억 하는 것이므로 한자리씩 바꾼다음 기저에서 검사한다
 
-			int res = 0;
+			long res = 0;
 			loop: for (int i = 0; i < binSize; i++) {
-				bin[i] = bAry[bin[i] - '0'];
-				res = changeBinToDec();
+				bin[i] = bAry[bin[i] - '0'][0];
+				res = parseDec(bin, 2);
 
 				for (int j = 0; j < third.length; j++) {
 					int cur = third[j] - '0';
-					int res2 = 0;
+					long res2 = 0;
 					third[j] = tAry[cur][0];
-					res2 = changeThrToDec();
+					res2 = parseDec(third, 3);
 					if (res == res2)
 						break loop;
 
 					third[j] = tAry[cur][1];
-					res2 = changeThrToDec();
+					res2 = parseDec(third, 3);
 					if (res == res2)
 						break loop;
 
 					third[j] = tAry[cur][2];
 				}
 
-				bin[i] = bAry[bin[i] - '0'];
+				bin[i] = bAry[bin[i] - '0'][0];
 			}
 
 			sb.append('#').append(test_case).append(' ').append(res).append('\n');
@@ -55,28 +55,11 @@ public class _4366_정식이의은행업무 {
 		System.out.print(sb);
 	}
 
-	private static int changeBinToDec() {
-		int ret = 0;
-		int val = 1;
-		for (int i = bin.length - 1; i >= 0; i--) {
-			if (bin[i] == '1') {
-				ret += val;
-			}
-			val *= 2;
-		}
-		return ret;
-	}
-
-	private static int changeThrToDec() {
-		int ret = 0;
-		int val = 1;
-		for (int i = third.length - 1; i >= 0; i--) {
-			if (third[i] == '1') {
-				ret += val;
-			} else if (third[i] == '2') {
-				ret += (val * 2);
-			}
-			val *= 3;
+	private static long parseDec(char[] target, int jinsu) {
+		long ret = 0;
+		int idx = 0;
+		for (int i = target.length - 1; i >= 0; i--) {
+			ret += Math.pow(jinsu, idx++) * (target[i] - '0');
 		}
 		return ret;
 	}
