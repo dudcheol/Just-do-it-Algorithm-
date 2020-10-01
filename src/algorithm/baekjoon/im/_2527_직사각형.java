bad79b3d55e9cc57982ea03c8c9c8cc93dda9aac
dudcheol <95_pyc@naver.com>
2020-10-01 18:42:27 +0900

    백준 / IM 검정 대비 / 직사각형 - 정답

--- src/algorithm/baekjoon/im/_2527_직사각형.java
+++ src/algorithm/baekjoon/im/_2527_직사각형.java
@@ -5,74 +5,32 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class _2527_직사각형 {
-
+	static int x1, y1, x2, y2, tx1, ty1, tx2, ty2;
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < 4; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			int p = Integer.parseInt(st.nextToken());
-			int q = Integer.parseInt(st.nextToken());
-
-			int x2 = Integer.parseInt(st.nextToken());
-			int y2 = Integer.parseInt(st.nextToken());
-			int p2 = Integer.parseInt(st.nextToken());
-			int q2 = Integer.parseInt(st.nextToken());
-
-			// 직사각형2의 각 꼭짓점
-			int[][] point = { { x2, y2 }, { p2, y2 }, { p2, q2 }, { x2, q2 } };
-
-			// 직사각형2의 각 꼭짓점이 직사각형1의 내부에 존재하는지 확인
-			boolean isD = true;
-			for (int d = 0; d < 4; d++) {
-				int target1 = point[d][0];
-				int target2 = point[d][1];
-
-				if ((x <= target1 && target1 <= p) && (y <= target2 && target2 <= q)) {
-					isD = false;
-					break;
-				}
-			}
-			if (isD) {
-				sb.append('d');
-				sb.append('\n');
-				continue;
-			}
-
-			boolean isA = true;
-			for (int d = 0; d < 4; d++) {
-				int target1 = point[d][0];
-				int target2 = point[d][1];
-
-				if (x == target1 || target1 == p || y == target2 || target2 == q) {
-					isA = false;
-					break;
-				}
-			}
-			if (isA) {
-				sb.append('a');
-				sb.append('\n');
-				continue;
-			}
-
-			int isB = 0;
-			for (int d = 0; d < 4; d++) {
-				int target1 = point[d][0];
-				int target2 = point[d][1];
-
-				if ((x == target1 || target1 == p) && (y == target2 || target2 == q)) {
-					isB++;
-					break;
-				}
-			}
-			if (isB == 1) {
-				sb.append('c');
-			} else if(isB > 1) {
-				sb.append('b');
-			}	
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x1 = Integer.parseInt(st.nextToken());
+            y1 = Integer.parseInt(st.nextToken());
+            x2 = Integer.parseInt(st.nextToken());
+            y2 = Integer.parseInt(st.nextToken());
+            tx1 = Integer.parseInt(st.nextToken());
+            ty1 = Integer.parseInt(st.nextToken());
+            tx2 = Integer.parseInt(st.nextToken());
+            ty2 = Integer.parseInt(st.nextToken());
+            
+            if ((x2 == tx1 && y2 == ty1) || (x1 == tx2 && y2 == ty1) || (x2 == tx1 && y1 == ty2) || (x1 == tx2 && y1 == ty2)) {
+                sb.append("c");
+            } else if ((x2 == tx1 && y2 != ty1) || (x1 == tx2 && y2 != ty1) || (x2 != tx1 && y1 == ty2) || (x1 != tx2 && y1 == ty2)) {
+                sb.append("b");
+            } else if (x2 < tx1 || tx2 < x1 || y2 < ty1 || ty2 < y1) {
+                sb.append("d");
+            } else {
+                sb.append("a");
+            }
+            
 			sb.append('\n');
 		}
 		System.out.println(sb);
