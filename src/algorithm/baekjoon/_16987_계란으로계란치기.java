bd0b9d04761d5bd1531a271df0bcdae327f0ec4f
dudcheol <95_pyc@naver.com>
2020-10-23 23:17:48 +0900

    백준 / 실버2 / 계란으로 계란치기 - 개선

--- src/algorithm/baekjoon/_16987_계란으로계란치기.java
+++ src/algorithm/baekjoon/_16987_계란으로계란치기.java
@@ -2,12 +2,11 @@ package algorithm.baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class _16987_계란으로계란치기 {
 
-	private static class Egg implements Comparable<Egg> {
+	private static class Egg {
 		int num;
 		int power;
 		int weight;
@@ -16,11 +15,6 @@ public class _16987_계란으로계란치기 {
 			this.num = num;
 			this.power = power;
 			this.weight = weight;
-		}
-
-		@Override
-		public int compareTo(Egg o) {
-			return Integer.compare(this.weight, o.weight);
 		}
 	}
 
@@ -38,20 +32,20 @@ public class _16987_계란으로계란치기 {
 			eggs[i] = new Egg(i, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
 		}
 
-		dfs(0, 0, 0);
+		dfs(0, 0);
 
 		System.out.println(answer);
 	}
 
-	private static void dfs(int cur, int cnt, int tryCnt) {
+	private static void dfs(int cur, int cnt) {
 		answer = Math.max(answer, cnt);
-		
-		if (tryCnt == N) {
+
+		if (cur == N) {
 			return;
 		}
 
 		if (eggs[cur].power <= 0) { // 현재 집은 계란이 깨져있으면 오른쪽 계란으로
-			dfs(cur + 1, cnt, tryCnt + 1);
+			dfs(cur + 1, cnt);
 			return;
 		}
 
@@ -59,27 +53,24 @@ public class _16987_계란으로계란치기 {
 			if (cur == i || eggs[i].power <= 0) // 현재 집고있는 계란 or 집었는데 이미 깨진 계란이면 건너뜀
 				continue;
 
-			Egg curTmp = new Egg(eggs[cur].num, eggs[cur].power, eggs[cur].weight);
-			Egg targetTmp = new Egg(eggs[i].num, eggs[i].power, eggs[i].weight);
 			eggs[cur].power -= eggs[i].weight;
 			eggs[i].power -= eggs[cur].weight;
 
 			if (eggs[cur].power <= 0) { // 손에 든 계란이 깨졌다면 들었던 계란의 오른쪽 계란을 집음
-				if (eggs[i].power <= 0) {
-					dfs(cur + 1, cnt + 2, tryCnt + 1); // 계란 둘 다 깨짐
-				} else {
-					dfs(cur + 1, cnt + 1, tryCnt + 1); // 현재 든 계란만 깨짐
-				}
-			} else { // 깨지지 않았다면 들고있는 계란 계속 들고서 계란깨기 진행
-				if (eggs[i].power <= 0) {
-					dfs(cur + 1, cnt + 1, tryCnt + 1); // 타겟 계란은 깨짐
-				} else {
-					dfs(cur + 1, cnt, tryCnt + 1); // 둘 다 안깨짐
-				}
-			}
+				if (eggs[i].power <= 0)
+					dfs(cur + 1, cnt + 2); // 계란 둘 다 깨짐
+				else
+					dfs(cur + 1, cnt + 1); // 현재 든 계란만 깨짐
 
-			eggs[cur] = curTmp;
-			eggs[i] = targetTmp;
+			} else { // 깨지지 않았다면 들고있는 계란 계속 들고서 계란깨기 진행
+				if (eggs[i].power <= 0)
+					dfs(cur + 1, cnt + 1); // 타겟 계란은 깨짐
+				else
+					dfs(cur + 1, cnt); // 둘 다 안깨짐
+			}
+			
+			eggs[cur].power += eggs[i].weight;
+			eggs[i].power += eggs[cur].weight;
 		}
 	}
 
