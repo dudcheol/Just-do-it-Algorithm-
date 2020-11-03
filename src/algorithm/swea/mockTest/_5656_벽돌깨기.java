b435a69776f0286865867cea2b96c1d1131084ff
dudcheol <95_pyc@naver.com>
2020-11-03 14:24:15 +0900

    swea / 모의 테스트 / 벽돌 깨기 - 개선2 1. 매 시도마다 남은 벽돌의 갯수를 세지 않고 남은 벽돌의 수를 가져오면서 dfs 진행

--- src/algorithm/swea/mockTest/_5656_벽돌깨기.java
+++ src/algorithm/swea/mockTest/_5656_벽돌깨기.java
@@ -10,6 +10,7 @@ public class _5656_벽돌깨기 {
 	static int[] dy = { -1, 1, 0, 0 };
 	static int[] dx = { 0, 0, -1, 1 };
 	private static int min;
+	private static ArrayList<Integer> list;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,15 +25,17 @@ public class _5656_벽돌깨기 {
 			H = Integer.parseInt(st.nextToken());
 			map = new int[H][W];
 			min = Integer.MAX_VALUE;
+			int total = 0;
 			for (int i = 0; i < H; i++) {
 				st = new StringTokenizer(br.readLine(), " ");
 				for (int j = 0; j < W; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
+					if(map[i][j] > 0) total++;
 				}
 			}
 
 			// 최대한 많은 블록을 깨서 남은 블록의 갯수 구하기 => 블록의 수가 가장 적게 남은 경우
-			dfs(0);
+			dfs(0, total);
 
 			sb.append('#').append(test_case).append(' ').append(min).append('\n');
 
@@ -42,16 +45,14 @@ public class _5656_벽돌깨기 {
 
 	}
 
-	private static boolean dfs(int k) {
-		int result = getRemain();// 남은 블록의 수 세기
-		
-		if(result == 0) {
+	private static boolean dfs(int k, int remainCnt) {
+		if(remainCnt == 0) {
 			min = 0;
 			return true; // 더이상 부술 벽돌이 없는 경우 dfs를 더 진행할 필요 없다
 		}
 		
 		if (k == N) {
-			min = Math.min(min, result);
+			min = Math.min(min, remainCnt);
 			return false;
 		}
 
@@ -59,32 +60,12 @@ public class _5656_벽돌깨기 {
 		deepcopy(map, tmp);
 
 		for (int i = 0; i < W; i++) {
-			bfs(i);
-			if(dfs(k + 1)) return true;
+			int boomCnt = bfs(i);
+			if(dfs(k + 1, remainCnt - boomCnt)) return true;
 			deepcopy(tmp, map);
 		}
+		
 		return false;
-	}
-
-	private static int getRemain() {
-		int cnt = 0;
-		for (int i = 0; i < H; i++) {
-			for (int j = 0; j < W; j++) {
-				if (map[i][j] != 0) {
-					cnt++;
-				}
-			}
-		}
-		return cnt;
-	}
-
-	// 계속해서 배열을 생성함 -> 메모리, 시간 낭비
-	private static int[][] deepcopy(int[][] origin) {
-		int[][] ret = new int[H][W];
-		for (int i = 0; i < H; i++) {
-			ret[i] = origin[i].clone();
-		}
-		return ret;
 	}
 	
 	// 기존에 존재하는 배열을 사용
@@ -96,7 +77,9 @@ public class _5656_벽돌깨기 {
 		}
 	}
 
-	private static void bfs(int x) {
+	private static int bfs(int x) {
+		int boomCnt = 0;
+
 		// k열의 0이 아닌 위치 찾기
 		int y = -1;
 		for (int i = 0; i < H; i++) {
@@ -105,14 +88,13 @@ public class _5656_벽돌깨기 {
 				break;
 			}
 		}
-
-		if (y == -1)
-			return; // 시도하지 않기
-
+		
+		if (y == -1) return boomCnt; // 시도하지 않기
+		
 		Queue<int[]> q = new LinkedList<>();
-		if (map[y][x] > 1)
-			q.offer(new int[] { y, x, map[y][x] - 1 });
+		if (map[y][x] > 1) q.offer(new int[] { y, x, map[y][x] - 1 });
 		map[y][x] = 0;
+		boomCnt++;
 
 		while (!q.isEmpty()) {
 			int[] p = q.poll();
@@ -135,40 +117,23 @@ public class _5656_벽돌깨기 {
 					if (ny < 0 || nx < 0 || ny >= H || nx >= W || map[ny][nx] == 0)
 						continue;
 
-					if (map[ny][nx] > 1)
-						q.offer(new int[] { ny, nx, map[ny][nx] - 1 });
+					if (map[ny][nx] > 1) q.offer(new int[] { ny, nx, map[ny][nx] - 1 });
 
 					map[ny][nx] = 0;
+					boomCnt++;
 				}
 			}
 		}
 
 		// 빈 칸 메꾸기
 		fillblank();
-	}
-
-	// 빈 칸 채우기 개선
-	private static void fillblank(){
-		// 모든열의 아래부터 빈칸 찾기
-		for (int c = 0; c < W; c++) { // 열고정
-			int r = H - 1;
-			while (r > 0) {
-				if (map[r][c] == 0) {
-					int nr = r - 1; // 직전행
-					while (nr > 0 && map[nr][c] == 0)
-						--nr; // 처음만나는 벽돌 찾기
-					map[r][c] = map[nr][c];
-					map[nr][c] = 0;
-				}
-				--r;
-			}
-		}
+		return boomCnt;
 	}
 	
 	// 빈 칸 채우기 더 쉬운 버전
-	private static void fillblank2(){
+	private static void fillblank(){
 		for (int c = 0; c < W; c++) { // 열고정
-			ArrayList<Integer> list = new ArrayList<>();
+			list = new ArrayList<>();
 			int r;
 			for (r = H-1; r >= 0; r--) { // 0이 아닌 벽돌 발견 시 리스트에 담음
 				if(map[r][c] > 0) {
@@ -181,6 +146,7 @@ public class _5656_벽돌깨기 {
 				map[--r][c] = b;
 			}
 		}
+		list.clear(); // 매번 생성하는 것 보다 메모리 사용량 효율 상승
 	}
 
 }
